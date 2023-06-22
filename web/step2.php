e4e991601839a96f3713d3c9d34a7e606b5cefae
hdewett <hdewett@bossinsights.com>
2023-06-22 17:52:22 -0400

    remove unused code

--- web/step2.php
+++ web/step2.php
@@ -10,7 +10,7 @@ use GuzzleHttp\Client;
 require __DIR__ . '/../common.php';
 
 if (!isset($_SESSION['account_key'])) {
-	header('Location: step1.php');
+	header('Location: index.php');
 }
 
 $selfSigned = (bool)getenv('SELF_SIGNED_CERT');
@@ -27,30 +27,15 @@ try {
 		]
 	]);
 } catch (Exception $e) {
-	// echo $twig->render('error.twig', array_merge($commonData, [
-	// 	'errorType'        => 'Error',
-	// 	'errorName'        => 'failed to communicate with the data api',
-	// 	'errorDescription' => 'Exception: ' . $e->getMessage()
-	// ]));
 	throw new Exception($e->getMessage(), $e->getCode(), $e);
 }
 //var_dump(json_decode($response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR));
 if ($response->getStatusCode() === 200) {
 	$result = json_decode($response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR);
 	if ($result === null) {
-		// echo $twig->render('error.twig', array_merge($commonData, [
-		// 	'errorType'        => 'Error',
-		// 	'errorName'        => 'invalid data api response',
-		// 	'errorDescription' => 'Response: ' . var_export($response->getBody()->getContents())
-		// ]));
 		throw new Exception('invalid data api response');
 	}
 } else {
-	// echo $twig->render('error.twig', array_merge($commonData, [
-	// 	'errorType'        => 'Error',
-	// 	'errorName'        => 'unable to communicate with data api',
-	// 	'errorDescription' => 'received status code ' . $response->getStatusCode() . ' when communicating with data api'
-	// ]));
 	throw new Exception('unable to communicate with data api');
 }
 $embedToken = $result['token'];
@@ -65,5 +50,3 @@ setcookie('site_url', $commonData['SITE_URL'], time() + (86400 * 30), "/");
 setcookie('embed_token', $embedToken, time() + (86400 * 30), "/");
 
 header('Location: step2_integration.html');
-
-// echo $twig->render('step2.twig', array_merge($commonData, ['EMBED_TOKEN' => $embedToken, 'ACCOUNT_DOMAIN' => $accountDomain]));
